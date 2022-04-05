e1b1da24433418ed7f027808497a9e8fa6904dcb
PtiBouchon <lopite2@live.fr>
2022-04-05 16:29:58 +0200

    Remove unused imports and useless comments

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use std::array::IntoIter;
-use std::collections::HashMap;
-use std::future::Future;
-use std::io::Cursor;
-use log::{info, LevelFilter, warn};
-use regex::{Regex, Match};
+use regex::Regex;
 use serenity::Client;
-use serenity::model::channel::{Channel, ChannelCategory, GuildChannel, Message, PartialGuildChannel, Reaction, ReactionType, StageInstance};
+use serenity::model::channel::{ChannelCategory, GuildChannel, Message, PartialGuildChannel, Reaction, ReactionType, StageInstance};
 use serenity::prelude::{EventHandler, Context};
 use serenity::async_trait;
-use serenity::http::Http;
 use serenity::model::gateway::{Activity, Ready};
-use serenity::model::prelude::{ChannelId, MessageId, User};
-use serenity::model::prelude::Action::Emoji;
 use serenity::utils::MessageBuilder;
 
 struct Regexes;
@@ -109,7 +101,7 @@ impl Handler {
         if let Some((m, i)) = Regexes::je_suis(&message) {
             if i <= index {
                 msg = Some(m);
-                index = i;
+                // index = i;
             }
         }
 
@@ -126,32 +118,6 @@ impl Handler {
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, message: Message) {
         self.message_sent(&ctx, message).await;
-        // POLICE -> Vous etes en état d'arrestation
-    //     let re = regex::Regex::new("di").unwrap();
-    //     if let Some(m) = re.find(&*msg.content) {
-    //         println!("Di !");
-    //         let mut bot_user = ctx.cache.current_user().await;
-    //
-    //         let url = msg.author.avatar_url().unwrap();
-    //         let img_bytes = reqwest::get(url).await.unwrap().bytes().await.unwrap();
-    //         let image = image::load_from_memory(&img_bytes).unwrap();
-    //         let mut buf = vec![];
-    //         image.write_to(&mut Cursor::new(&mut buf), image::ImageOutputFormat::Png).unwrap();
-    //         let res_base64 = base64::encode(buf);
-    //
-    //         let mut avatar = "data:image/png;base64,".to_owned();
-    //         avatar.push_str(&res_base64);
-    //
-    //         bot_user.edit(&ctx.http, |p| p.avatar(Some(&avatar))).await.unwrap();
-    //         info!("{}", &msg.author.avatar.unwrap());
-    //
-    //         if let Err(why) = msg.channel_id.say(&ctx.http, &msg.content[m.end()..]).await {
-    //             warn!("Error sending message: {}", why);
-    //         }
-    //
-    //         let avatar = serenity::utils::read_image("./avatar.png").unwrap();
-    //         bot_user.edit(&ctx.http, |p| p.avatar(Some(&avatar))).await.unwrap();
-    //     }
     }
 
     async fn ready(&self, ctx: Context, mut _data_about_bot: Ready) {
